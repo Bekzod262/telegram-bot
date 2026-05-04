b40e66142cef260e872f6b532084e97ffd652ddd
Bekzod <yoldoshevbekzod874@gmail.com>
2026-05-04 15:10:40 +0500

    update code

--- handlers.py
+++ handlers.py
@@ -1,91 +1,16 @@
-n = int(input("Son kiriting: "))
+msg = bot.send_message(chat_id, "Savol 1")
+bot.register_next_step_handler(msg,func1)
 
-sanoq = 0
+def func1(message):
+    javob1 = message.text
 
-while n > 0:
-    raqam = n %  10
-    sanoq += raqam
-    n = n // 10
-print(sanoq)
 
+    msg = 
+bot.send_message(message.chat.id,"Savol 2")
 
+bot.register_next_step_handler(msg,func2, javob1)
 
+def func2(message, javob1):
+    javob2 = message.text
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    bot.send_message(message.chat.id, f"1 - javob: {javob1}\n2-javob:{javob2}")
--- main.py
+++ main.py
@@ -1,360 +1,71 @@
-import telebot
-import os
-from dotenv import load_dotenv
+import telebot 
 
-load_dotenv()
-TOKEN =os.getenv("TOKEN")
-
-bot = telebot.TeleBot(TOKEN)
+bot = 
+telebot.TeleBot("8767281487:AAFKfLIpn9hZ2RtD_wbrs-F2dhGbEfHZKRI")
 
 
-@bot.message_handler(commands= ['start'])                                                                                                                                        
+@bot.message_handler(commands= ['start'])
 def start(message):
-    bot.send_message(message.chat.id," Salom Xush kelibsiz ZIKIR BOTMAN.>>>")
+    bot.send_message(message.chat.id,
+    "Salom!\n\n"
+    "1- Subhanalloh\n"
+    "2- Alhamdulillah\n"
+    "3- Allohu Akbar\n"
+    "4- La ilaha illalloh\n"
+    "5- Astaghfirulloh\n"
+    "6- SubhanaLLohi va bihamdihi\n"
+    "7- Subhanallohil azim\n"
+    "8- La hawla wa la quwwata"
+    "9- Salovat\n"
+    "10- Hasbiyallohu la ilaha illa hu")
 
+    # har qanday xabarni ushlaydi
 
 @bot.message_handler(func=lambda message: True)
 def handler(message):
     text = message.text
-    if text == "1":
 
-        bot.send_message(message.chat.id, "Subhanalloh")
-
+    if text ==  "1":
+        bot.send_message(message.chat.id,"Subhanalloh")
     elif text == "2":
-
-        bot.send_message(message.chat.id, "Alhamdulillah")
-    elif text == "3":
+        bot.send_message(message.chat.id,"Alhamdulilla")
+    elif text ==  "3":
         bot.send_message(message.chat.id,"Allohu Akbar")
-    
     elif text == "4":
-        bot.send_message(message.chat.id,"La ilaha illaloh")
-    
-    elif text == "5":
-        bot.send_message(message.chat.id,"Astaghfirulloh")
-    
+        bot.send_message(message.chat.id,"La ilaha illalloh")
+    elif  text == "5":
+        bot.send_message(message.chat.id, "Astaghfirulloh")
     elif text == "6":
-        bot.send_message(message.chat.id,"Subhanallohi va bihamdihi")
-
-    elif text ==  "7":
+        bot.send_message(message.chat.id, "Subhanallohi va bihamdihi")
+    elif text == "7":
         bot.send_message(message.chat.id, "Subhanallohil azim")
-    
-    elif text ==  "8":
-        bot.send_message(message.chat.id, "La hawla wa la quwwata illa bilah")
-
-    elif text ==  "9":
-        bot.send_message(message.chat.id, "Allahumma salli ala Muhammad")
-
+    elif text == "8":
+        bot.send_message(message.chat.id, "La hawla wa la quwwata illa billah")
+    elif text == "9":
+        bot.send_message(message.chat.id, "Allohumma salli ala Muhamamd")
     elif text == "10":
-        bot.send_message(message.chat.id, "Hasbiyallohu la ilaha illa Huwa")
+        bot.send_message(message.chat.id, "Hasbiyallohu la ilaha illa hu")
+    else:
+        bot.send_message(message.chat.id,"Iltimos 1 dan 10 gacha son yoz")
     
-                                                                       
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-bot.polling()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    bot.polling()
+                   
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    )
 
 
 
