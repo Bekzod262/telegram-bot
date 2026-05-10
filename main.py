4a743bff3bdb8e587fc9062e00941255d16a18a0
Bekzod <yoldoshevbekzod874@gmail.com>
2026-05-10 20:34:51 +0500

    update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telegram.ext import *
 async def start(update, context):
 
     await update.message.reply_text(
-        "Salom \nIsmingiz nima? "
+        "Salom 😎\nIsmingiz nima?"
     )
 
 async def chat(update, context):
@@ -11,26 +11,24 @@ async def chat(update, context):
     text = update.message.text
 
     await update.message.reply_text(
-        f"Voyy {text} juda zor ism ekan!"
+        f"Voyy {text} 😄 juda zor ism ekan!"
     )
 
     await update.message.reply_text(
-        f"{text} aka yoshingiz nechida ?"
+        f"{text} aka yoshingiz nechida? 🔥"
     )
 
     await update.message.reply_text(
-        "qanday ish qilasiz? "
+        "Qanday ish qilasiz? 😎"
     )
 
     await update.message.reply_text(
-        "siz juda zor odamsiz okam
-        "
+        "Siz juda zor odamsiz okam 🤣🔥"
     )
-app = 
-ApplicationBuilder().token("8767281487:AAG6T40XHhfpmvf4g4tpyIU6BPMUphx0kmc").build()
+
+app = ApplicationBuilder().token("8767281487:AAG6T40XHhfpmvf4g4tpyIU6BPMUphx0kmc").build()
 
 app.add_handler(CommandHandler("start", start))
-
 app.add_handler(MessageHandler(filters.TEXT, chat))
 
 app.run_polling()
