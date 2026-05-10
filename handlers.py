c1fa7a1d5ce9c3cb3c9f7fabcfc8cd5194073f49
Bekzod <yoldoshevbekzod874@gmail.com>
2026-05-10 19:49:38 +0500

    update

--- handlers.py
+++ handlers.py
@@ -1,58 +1,6 @@
-print("Assalomu alleykum xush kelibsiz: ")
-balans = 1500
-while True:
-    print("""
-    Tbc bank you are welkome
-    1---Royxatdan utish
-    2---malumot ollish
-    3---Aperaterga telfon qilish
-    4---summa qushish
-    5---summa ayirish
-    6---chiqish demak
-    7---boshiga qaytish demak
-    8---Tbc tarixi demak
-    9---kredit qancha ollish demak
-    10---I will try again""")
+parol  = input("Parolni kiriting: ")
 
-    tanlov = input("Tanlov : ")
-    if tanlov == "1":
-        bekzod = input("Son kiriting: ")
-        if bekzod != 0:
-            print("Davom etamiz demak bekzod:>>>")
-        else:
-            print("Buldi tugatamiz demak:>>>>>>>>>>")
-        print("""Ruyxatdan utish demak birinchi numer
-        kiratiz and va saqlab kirasiz demak""")
-    elif tanlov == "2":
-        print("""Malumot turlari demak
-        2 xill bulladi tanlaysiz jigarillo""")
-    elif tanlov == "3":
-        print(""" Aperatirga Boglanish demak
-        tugri anig ullab beramiz jigar""")
-    elif tanlov == "4":
-        summa  = int(input("Qancha balans qushishni xoxlaysiz"))
-        balans += summa
-        print(f"Balans { balans} uzgardi demak:>>")
-    elif tanlov == "5":
-        summa = int(input("Qancha balansni ayirishni xoxlaysiz"))
-        balans -= summa
-        print(f"Mufaqatli {balans} ayirildi demak:")
-    elif tanlov == "6":
-        print("chiqsh demak 6 bosing and chiq\di va qaytadi")
-    
-    elif tanlov == "7":
-        print("Boshiga qaytish demak bekzod:>>>")
-
-    elif tanlov == "8":
-        print("Tbc tarixi demak bekzod:>>>>")
-    
-    elif tanlov == "9":
-        print("Krdit 5milliondan 100milongacha ollish mumkin>>>")
-    elif tanlov == "10":
-        print("I will try agin do you understand bekzod>>>>>>>>>")
-        break
-    
-    else:
-        print("dastur tugadi demak:")
-        print("lets gooo bekzod Start bekzod")
-
+if parol == "1234":
+    print("Xush kelibsiz")
+else:
+    print("Xato parol")
--- main.py
+++ main.py
@@ -1,99 +1,28 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+from  telegram.ext import *
 
-balans = 2000
-savat = []
+async def start(update, context):
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "Xush kelibsiz 🛒\n"
-        "1 - Mahsulot\n"
-        "2 - Balans\n"
-        "3 - Qo‘shish\n"
-        "4 - Ayirish\n"
-        "5 - Chek\n"
-        "6 - Chiqish"
-    )
+    await
+update.message.reply_text("Salom")
+    await
+update.message.reply_text("Ismingizni kiriting: ")
+    await
 
-async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    global balans, savat
-    text = update.message.text.lower()
+async def chat(update, context):
 
-    if text == "1":
-        await update.message.reply_text(
-            "kepka-200\nfutbolka-300\nmuzqaymoq-100\ntelefon-800\nTanlang:"
-        )
+    ism = update.message.text
 
-    elif text in ["kepka", "futbolka", "muzqaymoq", "telefon"]:
-        narxlar = {
-            "kepka": 200,
-            "futbolka": 300,
-            "muzqaymoq": 100,
-            "telefon": 800
-        }
 
-        narx = narxlar[text]
+    await update.message.reply_text(f"salom {ism} aka qalaysiz")
 
-        if balans >= narx:
-            balans -= narx
-            savat.append((text, narx))
-            await update.message.reply_text(f"✅ Olindi. Balans: {balans}")
-        else:
-            await update.message.reply_text("❌ Pul yetarli emas")
+    await update.message.reply_text("Yoshingiz nechida?")
 
-    elif text == "2":
-        await update.message.reply_text(f"💰 Balans: {balans}")
-
-    elif text == "3":
-        context.user_data["state"] = "add_money"
-        await update.message.reply_text("Qancha qo‘shasiz?")
-
-    elif context.user_data.get("state") == "add_money":
-        try:
-            summa = int(text)
-            balans += summa
-            context.user_data["state"] = None
-            await update.message.reply_text(f"✅ Balans: {balans}")
-        except:
-            await update.message.reply_text("❌ Son yoz")
-
-    elif text == "4":
-        context.user_data["state"] = "minus_money"
-        await update.message.reply_text("Qancha ayirasiz?")
-
-    elif context.user_data.get("state") == "minus_money":
-        try:
-            summa = int(text)
-            if balans >= summa:
-                balans -= summa
-                await update.message.reply_text(f"✅ Balans: {balans}")
-            else:
-                await update.message.reply_text("❌ Yetarli emas")
-            context.user_data["state"] = None
-        except:
-            await update.message.reply_text("❌ Son yoz")
-
-    elif text == "5":
-        if not savat:
-            await update.message.reply_text("🧾 Savat bo‘sh")
-        else:
-            msg = "🧾 Chek:\n"
-            jami = 0
-            for item in savat:
-                msg += f"{item[0]} - {item[1]}\n"
-                jami += item[1]
-            msg += f"JAMI: {jami}"
-            await update.message.reply_text(msg)
-
-    elif text == "6":
-        await update.message.reply_text("👋 Xayr")
-
-    else:
-        await update.message.reply_text("❌ Noto‘g‘ri buyruq")
-
-app = ApplicationBuilder().token("8767281487:AAFdeqYyGOqmJJzZVL7HuKMfjUGBlrGvQp0").build()
+app = 
+ApplicationBuilder().token("8767281487:AAG6T40XHhfpmvf4g4tpyIU6BPMUphx0kmc").bu ild()
 
 app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT, message_handler))
 
-app.run_polling()
+app.add_handler(MessageHandler(filters.TEXT, chat))
+
+app.run_polling
+    
