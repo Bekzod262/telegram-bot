6468e36bdc892040c93f38747094beb29edfe533
Bekzod <yoldoshevbekzod874@gmail.com>
2026-05-10 20:12:15 +0500

    update

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-from telegram.ext import *
-
+from telegram.ext import * # kod boshlanishi ekan demak bekzod >>>>>>>>>>>>lets start bekzod 
+ #kichik kichik kichik mufaqatla katda galabalaga ollib keladi demak dustlarim
 async def start(update, context):
-
+# async def start(update, context): funksiya and enternet kutadi va qilla olladi demak bekzod lets start bekzod
     await update.message.reply_text("Salom 🙂")
     await update.message.reply_text("Ismingizni kiriting:")
 
 async def chat(update, context):
 
-    ism = update.message.text
-
+    ism = update.message.text # input urnini bosadi demak bekzod 
+# ism kirit deyabmiz demak  bekzod lets start bekzod (///////////>>>>>>><<<<<<<//////////)
     await update.message.reply_text(
-        f"Salom {ism} aka 😎 Qalaysiz?"
-    )
+        f"Salom {ism} aka 😎 Qalaysiz?")
 
-    await update.message.reply_text(
-        "Yoshingiz nechida?"
-    )
+async def chat(update, context):
+
+    age = update.message.text 
+
+    await update.message.reply_text(f"Sizning yoshingiz{age} aka 
+    siz zurisiz okam ")
 
 app = ApplicationBuilder().token("8767281487:AAG6T40XHhfpmvf4g4tpyIU6BPMUphx0kmc").build()
 
