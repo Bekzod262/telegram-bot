3c961d67b4f486b989a90ecc7358fe1b361f705a
Bekzod <yoldoshevbekzod874@gmail.com>
2026-05-11 10:37:50 +0500

    update

--- handlers.py
+++ handlers.py
@@ -1,6 +1,18 @@
-parol  = input("Parolni kiriting: ")
-
-if parol == "1234":
-    print("Xush kelibsiz")
-else:
-    print("Xato parol")
+print ("MINI KALKYATOR<<<<<")
+print("YOU ARE WELKOME ????????")
+while True:
+    print("""
+    1 --- Kopaytirish
+    2 --- Bollish
+    3 --- Ayirish
+    4 --- qoshish
+    5 --- chiqib ketish
+    6 --- foiz hisoblash
+    7 --- oxirgi raqamni topish
+    8 --- raqamlarni qoshish
+    9 --- bush savatda raqam and son qushish
+    10 --- bush savatda and yangi narsallar qushish
+    11 --- Login and parol yaratish
+    12 --- listdan xoxlagan son chiqarish
+    13 --- Dastur tugatish
+    14 --- Motivation gaplar///////>>>>>)
