fe87c07577ac2e99d3d5df52205a421e89d45599
Bekzod <yoldoshevbekzod874@gmail.com>
2026-05-13 19:53:32 +0500

    update

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text
 
     if user_id not in balanslar:
-        balanslar[user_id] = 800
+        balanslar[user_id] = 8000
 
     # 1
     if text == "1":
